6225d696c1a9971711b93072b2da0ff026f7e868
paulw <paulw@wilko.me>
2016-11-11 10:30:31 +1100

    Change product name

--- Example/Pods/Target Support Files/SubscriptionMonitor/SubscriptionMonitor-umbrella.h
+++ Example/Pods/Target Support Files/SubscriptionMonitor/SubscriptionMonitor-umbrella.h
@@ -2,6 +2,7 @@
 #import <UIKit/UIKit.h>
 #endif
 
+#import "SubscriptionMonitor.h"
 
 FOUNDATION_EXPORT double SubscriptionMonitorVersionNumber;
 FOUNDATION_EXPORT const unsigned char SubscriptionMonitorVersionString[];
