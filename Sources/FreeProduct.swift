9dbc6750b9e91b81f74ba0bae4f34cad0c0fa44c
paulw <paulw@wilko.me>
2016-11-12 16:12:41 +1100

    Update documentation

--- Sources/FreeProduct.swift
+++ Sources/FreeProduct.swift
@@ -9,13 +9,29 @@
 import Foundation
 import StoreKit
 
+/**
+ 
+    Represents a "free" product.  Free products have a perpetual duration.  They must have a `productID`
+    but unlike a `Product` this does not have to exist in iTunesConnect
+ */
+
 public class FreeProduct: Product {
+    
+    /// Indicates that this is a free product
     
     public override var isFree: Bool {
         get {
             return true
         }
     }
+    
+    /**
+        Initialise a new FreeProduct
+ 
+        - Parameters:
+            - productID: The productID associated with this product
+            - productLevel: The level associated with this product.  Typically lower that paid products in the same group
+     */
     
     public init(productID: String, productLevel: Int) {
         
